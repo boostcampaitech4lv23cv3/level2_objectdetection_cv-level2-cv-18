700bb6c9c4630305da007e9645b896cb6a2ef32d
HyeongSeok Kim <tiryul@gmail.com>
2022-11-22 11:11:16 +0000

    modify config: change name

--- configs/_test_/test_model.py
+++ configs/_test_/test_model.py
@@ -1,6 +1,6 @@
 _base_ = [
-    './_base_/datasets/coco_detection.py',
-    './_base_/models/faster_rcnn_r50_fpn.py',
-    './_base_/schedules/schedule_1x.py',
-    './_base_/default_runtime.py'
+    '../_main_/datasets/coco_detection.py',
+    '../_main_/models/faster_rcnn_r50_fpn.py',
+    '../_main_/schedules/schedule_1x.py',
+    '../_main_/default_runtime.py'
 ]
