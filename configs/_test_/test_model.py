aeda2b05a37ae627c9a8c8a06859f94f50da7b1b
HyeongSeok Kim <tiryul@gmail.com>
2022-11-22 11:11:44 +0000

    WIP

--- configs/_test_/test_model.py
+++ configs/_test_/test_model.py
@@ -1,6 +1,6 @@
 _base_ = [
-    '../_main_/datasets/coco_detection.py',
-    '../_main_/models/faster_rcnn_r50_fpn.py',
-    '../_main_/schedules/schedule_1x.py',
-    '../_main_/default_runtime.py'
+    '../_module_/datasets/coco_detection.py',
+    '../_module_/models/faster_rcnn_r50_fpn.py',
+    '../_module_/schedules/schedule_1x.py',
+    '../_module_/default_runtime.py'
 ]
