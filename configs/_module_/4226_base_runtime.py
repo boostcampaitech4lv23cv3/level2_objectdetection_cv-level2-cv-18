5610688805e55c926d191be9a8713b617e82a5b6
Sangjoon Han <jphan32@me.com>
2022-11-24 19:36:56 +0900

    Feature: Fastest learning

--- configs/_module_/4226_base_runtime.py
+++ configs/_module_/4226_base_runtime.py
@@ -1,10 +1,19 @@
-checkpoint_config = dict(interval=1)
+checkpoint_config = dict(interval=5)
 # yapf:disable
 log_config = dict(
     interval=50,
     hooks=[
         dict(type='TextLoggerHook'),
-        # dict(type='TensorboardLoggerHook'),
+        dict(type='MMDetWandbHook',
+             init_kwargs={
+                 'project': 'Trash Detection',
+                 "entity": "light-observer"
+             },
+             interval=10,
+             log_checkpoint=False,
+             log_checkpoint_metadata=True,
+             num_eval_images=100,
+             bbox_score_thr=0.3)
     ])
 # yapf:enable
 custom_hooks = [dict(type='NumClassCheckHook')]
@@ -13,10 +22,11 @@ dist_params = dict(backend='nccl')
 log_level = 'INFO'
 load_from = None
 resume_from = None
-workflow = [('train', 1)]
+workflow = [('train', 1), ]
 
 # disable opencv multithreading to avoid system being overloaded
-opencv_num_threads = 0
+#opencv_num_threads = 0
+opencv_num_threads = 16
 # set multi-process start method as `fork` to speed up the training
 mp_start_method = 'fork'
 
@@ -24,4 +34,5 @@ mp_start_method = 'fork'
 #   - `enable` means enable scaling LR automatically
 #       or not by default.
 #   - `base_batch_size` = (8 GPUs) x (2 samples per GPU).
-auto_scale_lr = dict(enable=False, base_batch_size=16)
+#auto_scale_lr = dict(enable=False, base_batch_size=16)
+auto_scale_lr = dict(enable=True, base_batch_size=4) # (2 GPUs) x (2 samples per GPU)
---
+++ configs/_module_/fp16/mixed_precision.py
@@ -0,0 +1,4 @@
+### Use Mixed Precision Training
+
+#fp16 = dict(loss_scale='dynamic')
+fp16 = dict(loss_scale=512.)
