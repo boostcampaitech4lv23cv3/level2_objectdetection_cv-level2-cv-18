503dbd8d4b4b0431e5123243ec948e53bc7a7892
teedihuni <dhlee132@gmail.com>
2022-11-25 23:16:53 +0900

    merge

---
+++ dist_train.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+CONFIG=$1
+# GPUS=$2
+GPUS=`nvidia-smi  -L | wc -l`
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
+# PORT=${PORT:-29500}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+
+# #    --master_port=$PORT \
+# --master_addr=$MASTER_ADDR \
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch \
+    --nnodes=$NNODES \
+    --node_rank=$NODE_RANK \
+    --master_addr=172.17.0.2 \
+    --nproc_per_node=$GPUS \
+    --master_port=32000 \
+    $(dirname "$0")/train.py \
+    $CONFIG \
+    --seed 0 \
+    --launcher pytorch ${@:3}
